c38ef632183d32aa8c27685dadd24ed6e2504e55
Maciej Opaliński <poprostumieciek@gmail.com>
2022-10-20 13:48:04 +0200

    fix: Check if docker volume exists in `backup.sh`

--- backup.sh
+++ backup.sh
@@ -1,10 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
 VOLUME_NAME="$1"
 BACKUP_FILE="$2"
 
+if [[ -z "$VOLUME_NAME" ]]; then
+    echo "Error: Please provide docker volume name"
+    exit 1
+fi
+
+if [[ -z "$(docker volume ls -q | grep -x "$VOLUME_NAME")" ]]; then
+    echo "Error: Docker volume not found"
+    exit 1
+fi
+
+if [[ -z "$BACKUP_FILE" ]]; then
+    BACKUP_FILE="$VOLUME_NAME.tar.gz"
+fi
+
 docker run --rm \
     -v $VOLUME_NAME:/volume \
-    -v `pwd`:/backup \
+    -v $(pwd):/backup \
     ubuntu \
     tar czvf /backup/$BACKUP_FILE /volume
