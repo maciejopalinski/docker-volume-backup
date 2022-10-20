17fcd0ba2604bc340e0f48f011f4776ac50aba8d
Maciej Opaliński <poprostumieciek@gmail.com>
2022-10-20 13:48:17 +0200

    fix: Check if backup file exists in `backup.sh`

--- restore.sh
+++ restore.sh
@@ -1,7 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 BACKUP_FILE="$1"
 VOLUME_NAME="$2"
+
+if [[ -z "$BACKUP_FILE" ]]; then
+    echo "Error: Please provide backup file name"
+    exit 1
+fi
+
+if [[ ! -f "$BACKUP_FILE" ]]; then
+    echo "Error: File not found"
+    exit 1
+fi
+
+if [[ -z "$VOLUME_NAME" ]]; then
+    VOLUME_NAME=${BACKUP_FILE%.tar.gz}
+fi
 
 docker run --rm \
     -v $VOLUME_NAME:/volume \
